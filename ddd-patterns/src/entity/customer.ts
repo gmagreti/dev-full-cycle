b323a6c608a8eb1f16d45c714924202994421af5
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-09-21 20:34:11 -0300

    Testando Customer

--- ddd-patterns/src/entity/customer.ts
+++ ddd-patterns/src/entity/customer.ts
@@ -13,10 +13,10 @@ import Address from "./address";
 
 export class Customer {
 
-  _id: string;
-  _name: string;
-  _address!: Address;
-  _active: boolean = false;
+  private _id: string;
+  private _name: string;
+  private _address!: Address;
+  private _active: boolean = false;
 
   constructor(id: string, name: string) {
     this._id = id;
@@ -24,21 +24,15 @@ export class Customer {
     this.validate()
   }
 
+  get name(): string { return this._name; }
+
   validate() {
     if (this._id.length === 0) {
-      throw new Error('_id is required')
+      throw new Error('Id is required')
     }
 
     if (this._name.length === 0) {
-      throw new Error('_name is required')
-    }
-
-    if (this._address === undefined) {
-      throw new Error('_address is required')
-    }
-
-    if (!this._active) {
-      throw new Error('_active is required')
+      throw new Error('Name is required')
     }
   }
 
@@ -46,7 +40,12 @@ export class Customer {
     this._name = name;
   }
 
+  isActive(): boolean { return this._active; }
+
   activate() {
+    if(this._address === undefined) {
+      throw new Error('Address is mandatory to activate a customer')
+    }
     this._active = true;
   }
 
